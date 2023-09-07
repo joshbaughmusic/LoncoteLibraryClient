0cba11f23811e8981b346c80d618ab50a182fd02
Josh Baugh <joshbaughmusic@gmail.com>
2023-09-07 14:32:47 -0500

    features 6 and 7

---
+++ src/data/checkoutsData.js
@@ -0,0 +1,12 @@
+const _apiUrl = '/api/checkouts';
+
+export const getCheckouts = () => {
+  return fetch(_apiUrl).then((res) => res.json());
+};
+
+export const returnCheckout = (id) => {
+    return fetch(`${_apiUrl}/${id}/return`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'}
+    })
+}
